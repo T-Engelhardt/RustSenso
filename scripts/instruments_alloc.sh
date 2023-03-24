8ed02d1ad1dd43db30def6dfe93fd0d968fdd68d
Tjark Engelhardt <tjark.engelhardt@pm.me>
2023-03-24 10:10:43 +0100

    move serial to secret

--- scripts/instruments_alloc.sh
+++ scripts/instruments_alloc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 source secret
-cargo instruments -t Allocations --bin $1 -- -s 21223900202609620938071939N6 --user $VaillantAppUSER --pwd $VaillantAppPWD
+cargo instruments -t Allocations --bin $1 -- -s $VaillantSerial --user $VaillantAppUSER --pwd $VaillantAppPWD
--- scripts/run_cargo.sh
+++ scripts/run_cargo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 source secret
-cargo run --bin $1 -- -s 21223900202609620938071939N6 --user $VaillantAppUSER --pwd $VaillantAppPWD
+cargo run --bin $1 -- -s $VaillantSerial --user $VaillantAppUSER --pwd $VaillantAppPWD
